0b43180b391a7922ce0e0c58a79eefb73453faee
Jérémie Hatier <143613147+Jhatier@users.noreply.github.com>
2025-08-26 09:42:27 -0400

    Update génération de la CODE_MAP

--- functions/api/validate-code.js
+++ functions/api/validate-code.js
@@ -1,11 +1,10 @@
+import STATIONS from 'src/lib/App.jsx'
+
 // Simple table côté serveur (tu peux la déplacer plus tard en KV/D1)
-const CODE_MAP = {
-  S01: 'UL-001', S02: 'UL-002', S03: 'UL-003', S04: 'UL-004', S05: 'UL-005',
-  S06: 'UL-006', S07: 'UL-007', S08: 'UL-008', S09: 'UL-009', S10: 'UL-010',
-  S11: 'UL-011', S12: 'UL-012', S13: 'UL-013', S14: 'UL-014', S15: 'UL-015',
-  S16: 'UL-016', S17: 'UL-017', S18: 'UL-018', S19: 'UL-019', S20: 'UL-020',
-  S21: 'UL-021',
-};
+const CODE_MAP = STATIONS.reduce((map, station) => {
+  map[station.id] = station.code;
+  return map;
+}, {});
 
 export const onRequestPost = async ({ request }) => {
   try {
